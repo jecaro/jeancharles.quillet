0ef7007a9767873205f0a136d62007888e6ed88e
Jean-Charles Quillet <jeancharles.quillet@gmail.com>
2021-02-07 21:33:08 +0100

    add hakyll default site

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  project = import ./. { inherit pkgs; };
+in
+with pkgs;
+mkShell {
+  inputsFrom = [ project.env ];
+  buildInputs = [
+    ghcid
+    haskellPackages.cabal-install
+    haskellPackages.haskell-language-server
+    haskellPackages.hlint
+    haskellPackages.implicit-hie
+  ];
+}
