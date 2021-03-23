32fc5caa5bdb92ca886621bc2f2451dd7acb3563
Jean-Charles Quillet <jeancharles.quillet@gmail.com>
2021-03-23 21:41:43 +0100

    check that the dir is clean before uploading it

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,16 @@ set -o xtrace
 set -o nounset
 set -o errexit
 
+# Check that the directory is clean
+# https://unix.stackexchange.com/a/394674
+git diff-index --quiet HEAD
+
+# Build the site
 cabal run site -- clean
 cabal run site -- build
+
+# Check that the links are good
 cabal run site -- check
 
+# Upload on the server
 rsync -avh --delete _site/ jeancharles.quillet.org:site
