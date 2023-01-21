81c7c9832ab0ea11f881dbc840db07e3cf34f3db
jecaro <jeancharles.quillet@gmail.com>
2023-01-21 16:52:06 +0100

    add the drv to build the website itself

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
-  # description = "A Hello World in Haskell with a dependency and a devShell";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
   outputs = { self, nixpkgs }:
     let
@@ -12,19 +10,45 @@
       });
     in
     {
-      overlay = (final: prev: {
-        jeancharles-quillet = final.haskellPackages.callCabal2nix "jeancharles-quillet" ./. { };
-      });
+      overlay = (final: prev:
+        {
+          site = final.haskellPackages.callCabal2nix "site" ./. { };
+          jeancharles-quillet = final.stdenv.mkDerivation {
+            name = "jeancharles-quillet";
+            src = ./.;
+
+            LC_ALL = "C.UTF-8";
+
+            buildInputs = [ final.site ];
+            buildPhase = ''
+              ${final.site}/bin/site build
+            '';
+
+            doCheck = true;
+            checkPhase = ''
+              ${final.site}/bin/site check
+            '';
+
+            installPhase = ''
+              cp -r _site $out
+            '';
+          };
+        });
+
       packages = forAllSystems (system: {
+        site = nixpkgsFor.${system}.site;
         jeancharles-quillet = nixpkgsFor.${system}.jeancharles-quillet;
       });
+
       defaultPackage = forAllSystems (system: self.packages.${system}.jeancharles-quillet);
+
       checks = self.packages;
+
       devShell = forAllSystems (system:
         let haskellPackages = nixpkgsFor.${system}.haskellPackages;
         in
         haskellPackages.shellFor {
-          packages = p: [ self.packages.${system}.jeancharles-quillet ];
+          packages = p: [ self.packages.${system}.site ];
           withHoogle = true;
           buildInputs = with haskellPackages; [
             haskell-language-server
